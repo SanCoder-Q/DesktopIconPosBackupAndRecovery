5de67969b5b17348116ee4e14b59b9ad26b6dbbd
SanCoder-Q <sancoder.q@gmail.com>
2014-09-24 18:00:53 +0800

    First executable version

---
+++ SanCoder.Widget.DesktopIconPosBackupAndRecovery.Client/DesktopIconControl.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using SanCoder.Widget.DesktopIconPosBackupAndRecovery.Util;
+
+namespace SanCoder.Widget.DesktopIconPosBackupAndRecovery
+{
+    public static class DesktopIconControl
+    {
+        private static List<Icon> GetIconInfo()
+        {
+            List<Icon> aIcons = new List<Icon>();
+
+            //取桌面ListView句柄
+            IntPtr hDeskTop = WinAPI.FindWindow("progman", null);
+            hDeskTop = WinAPI.FindWindowEx(hDeskTop, IntPtr.Zero, "shelldll_defview", null);
+            hDeskTop = WinAPI.FindWindowEx(hDeskTop, IntPtr.Zero, "syslistview32", null);
+
+            //取explorer.exe句柄
+            int dwProcessId;
+            WinAPI.GetWindowThreadProcessId(hDeskTop, out dwProcessId);
+            IntPtr hProcess = WinAPI.OpenProcess(WinAPI.PROCESS_VM_OPERATION | WinAPI.PROCESS_VM_READ | WinAPI.PROCESS_VM_WRITE, false, dwProcessId);
+
+
+            unsafe
+            {
+                //在 explorer.exe 空间内分配内存
+                IntPtr pv = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, sizeof(Point), WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+                IntPtr pn = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, WinAPI.MAX_PATH * 2, WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+                IntPtr plvitem = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, WinAPI.GetLvItemSize(), WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+
+                //发送获取坐标消息
+                int count = WinAPI.ListView_GetItemCount(hDeskTop);
+
+                for (int i = 0; i < count; i++)
+                {
+                    Icon icon = new Icon();
+                    WinAPI.ListView_GetItemText(hDeskTop, i, hProcess, plvitem, (int)pn, WinAPI.MAX_PATH * 2);
+
+                    WinAPI.ListView_GetItemPosition(hDeskTop, i, pv);
+
+                    //读取坐标数据
+                    int lpNumberOfBytesRead;
+                    WinAPI.ReadProcessMemory(hProcess, pv, out icon.Position, sizeof(Point), out lpNumberOfBytesRead);
+
+                    //读取文本数据
+                    IntPtr pszName = Marshal.AllocCoTaskMem(WinAPI.MAX_PATH*2);
+                    WinAPI.ReadProcessMemory(hProcess, pn, pszName, WinAPI.MAX_PATH * 2, out lpNumberOfBytesRead);
+                    icon.Text = Marshal.PtrToStringUni(pszName);
+                    Marshal.FreeCoTaskMem(pszName);
+
+                    //加入列表
+                    aIcons.Add(icon);
+
+                    Console.Out.WriteLine(icon.Text);
+                    //int result = Marshal.GetLastWin32Error();
+                }
+
+                //释放内存
+                WinAPI.VirtualFreeEx(hProcess, pv, sizeof(Point), 0);
+                WinAPI.VirtualFreeEx(hProcess, pn, WinAPI.MAX_PATH * 2, 0);
+                WinAPI.VirtualFreeEx(hProcess, plvitem, WinAPI.GetLvItemSize(), 0);
+
+                //释放句柄
+                WinAPI.CloseHandle(hProcess);
+            }
+            return aIcons;
+        }
+
+        private static void SetIconInfo(List<Icon> aIcons)
+        {
+            if (aIcons == null) throw new ArgumentNullException("aIcons");
+            if (aIcons.Count == 0) return;
+
+            //取桌面ListView句柄
+            IntPtr hDeskTop = WinAPI.FindWindow("progman", null);
+            hDeskTop = WinAPI.FindWindowEx(hDeskTop, IntPtr.Zero, "shelldll_defview", null);
+            hDeskTop = WinAPI.FindWindowEx(hDeskTop, IntPtr.Zero, "syslistview32", null);
+
+            //取explorer.exe句柄
+            int dwProcessId;
+            WinAPI.GetWindowThreadProcessId(hDeskTop, out dwProcessId);
+            IntPtr hProcess = WinAPI.OpenProcess(WinAPI.PROCESS_VM_OPERATION | WinAPI.PROCESS_VM_READ | WinAPI.PROCESS_VM_WRITE, false, dwProcessId);
+
+            unsafe
+            {
+                //在 explorer.exe 空间内分配内存
+                IntPtr pv = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, sizeof(Point), WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+                IntPtr pn = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, WinAPI.MAX_PATH * 2, WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+                IntPtr plvitem = WinAPI.VirtualAllocEx(hProcess, IntPtr.Zero, WinAPI.GetLvItemSize(), WinAPI.MEM_COMMIT, WinAPI.PAGE_READWRITE);
+
+                //发送获取坐标消息
+                int count = WinAPI.ListView_GetItemCount(hDeskTop);
+
+                for (int i = 0; i < count; i++)
+                {
+                    WinAPI.ListView_GetItemText(hDeskTop, i, hProcess, plvitem, (int)pn, WinAPI.MAX_PATH * 2);
+
+                    //读取文本数据
+                    IntPtr pszName = Marshal.AllocCoTaskMem(WinAPI.MAX_PATH * 2);
+                    int lpNumberOfBytesRead;
+                    WinAPI.ReadProcessMemory(hProcess, pn, pszName, WinAPI.MAX_PATH * 2, out lpNumberOfBytesRead);
+                    string text = Marshal.PtrToStringUni(pszName);
+                    Marshal.FreeCoTaskMem(pszName);
+
+                    //判断包含列表
+                    Icon icon = aIcons.SingleOrDefault(item => item.Text == text);
+                    if (icon == null)
+                        continue;
+
+                    //设置坐标
+                    WinAPI.ListView_SetItemPosition(hDeskTop, i, icon.Position.x, icon.Position.y);
+                }
+
+                //释放内存
+                WinAPI.VirtualFreeEx(hProcess, pv, sizeof(Point), 0);
+                WinAPI.VirtualFreeEx(hProcess, pn, WinAPI.MAX_PATH * 2, 0);
+                WinAPI.VirtualFreeEx(hProcess, plvitem, WinAPI.GetLvItemSize(), 0);
+
+                //释放句柄
+                WinAPI.CloseHandle(hProcess);
+            }
+
+        }
+
+
+        public static void BackupIconInfo(string backupfilepath)
+        {
+            try
+            {
+                if (!Directory.Exists(backupfilepath.Substring(0, backupfilepath.LastIndexOf('\\'))))
+                {
+                    Console.Out.WriteLine("Error: 备份文件地址<" + backupfilepath + ">不存在");
+                    Console.In.ReadLine();
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+            }
+
+            //二进制序列化
+            FileStream fs = File.Open(backupfilepath, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, GetIconInfo());
+            fs.Close();
+        }
+
+        public static void BackupIconInfo()
+        {
+            //获取文档路径
+            BackupIconInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\DesktopIconPositionSave.dat");
+        }
+
+        public static void RecoveryIconInfo()
+        {
+            //获取文档路径
+            RecoveryIconInfo(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\DesktopIconPositionSave.dat");
+        }
+
+        public static void RecoveryIconInfo(string backupfilepath)
+        {
+            try
+            {
+                if (!File.Exists(backupfilepath))
+                {
+                    Console.Out.WriteLine("Error: 备份文件地址<" + backupfilepath + ">不存在");
+                    Console.In.ReadLine();
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+            }
+
+            FileStream fs = File.Open(backupfilepath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            SetIconInfo((List<Icon>) bf.Deserialize(fs));
+        }
+
+        public static void ShowHelp()
+        {
+            Console.Out.WriteLine("");
+            Console.Out.WriteLine("===============================================");
+            Console.Out.WriteLine("桌面图标位置备份与恢复程序, SanCoder 2014-09-24");
+            Console.Out.WriteLine("-----------------------------------------------");
+            Console.Out.WriteLine("命令格式:");
+            Console.Out.WriteLine("\t -b [BackupFilePath]  备份图标位置");
+            Console.Out.WriteLine("\t -r [BackupFilePath]  恢复图标位置");
+            Console.Out.WriteLine("===============================================");
+            Console.Out.WriteLine("");
+        }
+
+    }
+
+    [Serializable]
+    public struct Point
+    {
+        public int x;
+        public int y;
+    }
+
+    [Serializable]
+    public class Icon
+    {
+        public string Text;
+        public Point Position;
+    }
+}
---
+++ SanCoder.Widget.DesktopIconPosBackupAndRecovery.Client/WinAPI.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SanCoder.Widget.DesktopIconPosBackupAndRecovery.Util
+{
+    public static class WinAPI
+    {
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        public extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx", SetLastError = true)]
+        public extern static IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("kernel32.dll", EntryPoint = "VirtualAllocEx", SetLastError = true)]
+        public extern static IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, int flAllocationType, int flProtect);
+
+        [DllImport("kernel32.dll", EntryPoint = "VirtualFreeEx", SetLastError = true)]
+        public static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress, int dwSize, int dwFreeType);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll", EntryPoint = "GetWindowThreadProcessId", SetLastError = true)]
+        public static extern int GetWindowThreadProcessId(IntPtr hWnd, out int ProcessId);
+
+        [DllImport("kernel32.dll", EntryPoint = "OpenProcess", SetLastError = true)]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", EntryPoint = "CloseHandle", SetLastError = true)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", EntryPoint = "ReadProcessMemory", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, out Point lpBuffer, int nSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", EntryPoint = "ReadProcessMemory", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", EntryPoint = "WriteProcessMemory", SetLastError = true)]
+        public static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, int nSize, out int lpNumberOfBytesWritten);
+
+        public static int ListView_GetItemPosition(IntPtr hwndLV, int i, IntPtr ppt)
+        {
+            return SendMessage(hwndLV, LVM_GETITEMPOSITION, i, ppt);
+        }
+
+        public static int ListView_SetItemPosition(IntPtr hwndLV, int i, int x, int y)
+        {
+            return SendMessage(hwndLV, LVM_SETITEMPOSITION, i, x | ((UInt16)y << 16));
+        }
+
+        public static int ListView_GetItemCount(IntPtr hwndLV)
+        {
+            return SendMessage(hwndLV, LVM_GETITEMCOUNT, 0, 0);
+        }
+
+        public static int ListView_GetItemText(IntPtr hwndLV, int i, IntPtr hProcess, IntPtr plvitem, int pszText_, int cchTextMax)
+        {
+            //建立结构
+            tagLVITEMW _macro_lvi = new tagLVITEMW();
+            _macro_lvi.iSubItem = 0;
+            _macro_lvi.cchTextMax = cchTextMax;
+            _macro_lvi.pszText = pszText_;
+
+            //转换到非托管区
+            IntPtr lv = Marshal.AllocCoTaskMem(GetLvItemSize());
+            Marshal.StructureToPtr(_macro_lvi, lv, false);
+
+            //目标进程分配空间填充
+            int lpNumberOfBytesWritten;
+            int result = WriteProcessMemory(hProcess, plvitem, lv, GetLvItemSize(), out lpNumberOfBytesWritten);
+            Marshal.FreeCoTaskMem(lv);
+            //Console.Out.WriteLine(result);
+            return SendMessage(hwndLV, LVM_GETITEMTEXT, i, plvitem);
+        }
+
+        public const int PROCESS_VM_READ = 0x0010;
+        public const int PROCESS_VM_OPERATION = 0x0008;
+        public const int PROCESS_VM_WRITE = 0x0020;
+
+        public const int MEM_COMMIT = 0x1000;
+        public const int PAGE_READWRITE = 0x04;
+
+        public const int LVM_GETITEMPOSITION = 0x1010;
+        public const int LVM_SETITEMPOSITION = 0x100F;
+        public const int LVM_GETITEMCOUNT = 0x1004;
+        public const int LVM_GETITEMTEXT = 0x1073;
+
+        public const int MAX_PATH = 260;
+
+        public struct tagLVITEMW
+        {
+            public uint mask;
+            public int iItem;
+            public int iSubItem;
+            public uint state;
+            public uint stateMask;
+            public Int64 pszText;
+            public int cchTextMax;
+            public int iImage;
+            public int lParam;
+            public int iIndent;
+            public int iGroupId;
+            public uint cColumns; // tile view columns
+            public IntPtr puColumns;
+            public IntPtr piColFmt;
+            public int iGroup; // readonly. only valid for owner data.
+        }
+
+        public static int GetLvItemSize()
+        {
+            return Marshal.SizeOf(typeof(tagLVITEMW));
+        }
+    }
+}
